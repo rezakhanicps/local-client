4a4b3902710049ff3369db8a225656bb90bae3c6
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-10-21 17:11:52 +0330

    Debouncing Save Logic

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistMiddleware } from './middlewares/persist-middleware';
 import reducers from './reducers';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(persistMiddleware, thunk)
+);
